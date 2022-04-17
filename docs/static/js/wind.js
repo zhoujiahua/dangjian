3338453c19804a2b102224d16d0f757a15e216e6
jiahua <jiahuasir@163.com>
2022-04-18 04:11:01 +0800

    Auto commit code

--- docs/static/js/wind.js
+++ docs/static/js/wind.js
@@ -4,10 +4,17 @@ $(function () {
         $(".offline").show();
         $(".login").hide();
         localStorage.setItem("isLogin", false);
-        window.location.href = '/user/login/index.html'
+        window.location.href = '/user/login/index.html';
     } else {
         $(".login").show();
         $(".offline").hide();
         localStorage.setItem("isLogin", true);
     }
+
+    $("#logout").on("click", function () {
+        localStorage.removeItem("isLogin");
+        window.location.href = '/user/login/index.html';
+    })
 })
+
+
